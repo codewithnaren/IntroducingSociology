8a566820af878ffa185252c094a194dad483c6e2
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-09-29 17:38:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/IntroducingSociology/IntroducingSociology.pdf';
 
 
 let pdfDoc = null,
